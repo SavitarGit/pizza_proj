e539cabb7661d2e04f87362f9a8f96aea33bcae5
Daniil <kolganovdaniilpy@gmail.com>
2022-05-23 20:10:41 +0300

    done (пока без возможности изменять количество товаров в корзине и удалять товары из корзины)

--- js/totalCart.js
+++ js/totalCart.js
@@ -3,18 +3,17 @@ function calcCartPriceAndDelivery() {
     const priceElements = cartWrapper.querySelectorAll('.pizza-block__price');
     const totalPriceEl = document.querySelector('.total-price');
     const totalElements = document.querySelector('.total-elem');
-    // Общая стоимость товаров
+
     let priceTotal = 0;
     let elemenTotal =0;
 
-    // Обходим все блоки с ценами в корзине
+
     priceElements.forEach(function (item) {
-        // Добавляем стоимость товара в общую стоимость (кол-во * цену)
         priceTotal += parseInt(item.innerText);
         elemenTotal = (cartWrapper.children.length)
     });
 
-    // Отображаем цену на странице
+
     totalPriceEl.innerText = priceTotal;
     totalElements.innerText = elemenTotal;
 }
